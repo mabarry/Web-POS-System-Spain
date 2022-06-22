5d8743855cccc059a703fddd5ff139affd002449
Matthew Barry <90521761+mabarry@users.noreply.github.com>
2022-06-22 12:42:25 -0500

    Added button functionalities
    'Enter' key provides functionality, add items to table, remove items from table completed. All members present and contributed to code.

--- bootstrap/scripts.js
+++ bootstrap/scripts.js
@@ -1,19 +1,19 @@
 var accessibility = false;
 
-
-function clearBox(x) {
-    document.getElementById(x).value = "";
-}
-
-
 function clearBoxes() {
-    document.getElementById('foodID').value = "";
-    document.getElementById('foodQuantity').value = "";
+    document.getElementById("foodID").value = "";
+    document.getElementById("foodQuantity").value = "";
 }
-
+/*
+function searchFunction() {
+    //document.getElementById(x).value = "";
+    document.getElementById("foodID").blur();
+    document.getElementById("foodQuantity").focus();
+}
+*/
 
 function searchFunction(x, y) {
-    document.getElementById(x).value = "";
+    //document.getElementById(x).value = "";
     document.getElementById(x).blur();
     document.getElementById(y).focus();
 }
@@ -22,9 +22,11 @@ function searchFunction(x, y) {
 function accessibleMode() {
     if(accessibility) {
         document.getElementById('accessibilityButton').setAttribute('class', 'accessibility');
+        accessibility = false;
     }
     else {
         document.getElementById('accessibilityButton').setAttribute('class', 'accessibilityOn');
+        accessibility = true;
     }
 }
 
@@ -36,10 +38,14 @@ function deleteRow(btn) {
 
 
 function addRow() {
-    var name = "TestFood (#56)";
+    var name = 'exampleFood (' + document.getElementById('foodID').value + ')';
     var price = "$9.99";
-    var qty = "10"
-    
+    var qty = document.getElementById('foodQuantity').value;
+
+    if (document.getElementById('foodID').value === '' || document.getElementById('foodQuantity').value === '') {
+        return;
+    }
+
     // Create new row
     var tr = document.createElement('tr');
 
@@ -54,4 +60,25 @@ function addRow() {
     tr.innerHTML = rowText;
     var table = document.getElementById("orders")
     table.appendChild(tr);
+
+    //Clearing text boxes containing food ID and quantity
+    clearBoxes();
+}
+
+
+function completeOrder() {
+    var table = document.getElementById("orders");
+
+    // Get the payment info for the order
+    // ...
+
+    /*
+    If there was connectivity to the database, this is where we would update the final order info
+    */
+
+    // Delete all rows
+    var tableLen = table.rows.length;
+    for (var i = 1; i < tableLen; i++) {
+        table.deleteRow(1);
+    }
 }
