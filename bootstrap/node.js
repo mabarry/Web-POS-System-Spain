a981858126373debd0784306047cd8504473840d
KyleDOwens <90162505+KyleDOwens@users.noreply.github.com>
2022-06-28 15:12:13 +0200

    Updated node.js

--- bootstrap/node.js
+++ bootstrap/node.js
@@ -1,26 +1,41 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
 var path = require('path');
 
-var filePath = path.join(__dirname, 'public');
 
-app.use(express.static(filePath));
-
-// Connect to database
-const {Client} = require('pg');
-const client = new Client({
-    host: "csce-315-db.engr.tamu.edu",
-    user: "csce315950_1user",
-    port: 5432,
-    password: "team1won",
-    database: "csce315950_1db"
-})
-client.connect();
-
+app.use(cors({origin: "*"}));
+app.use(express.static('public'));
 
 app.listen(port, () => {
   console.log(`Point of Sales system listening on port ${port}`)
 })
 
 
+const {Client} = require('pg');
+const client = new Client({
+  host: "csce-315-db.engr.tamu.edu",
+  user: "csce315950_1user",
+  port: 5432,
+  password: "team1won",
+  database: "csce315950_1db"
+})
+
+var foodItems;
+client.connect();
+client.query('SELECT * FROM fooditems ORDER BY foodid ASC',(err, res)=>{
+  if (!err) {
+    foodItems = res.rows
+    console.log(res.rows);
+  } else {
+    console.log("\nERROR:");
+    console.log(err.message);
+  }
+  client.end();
+})
+
+
+app.get('/foodItems', function(req, res){
+    res.send(foodItems);
+});
