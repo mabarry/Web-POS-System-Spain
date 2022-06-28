1021440ff74c7da6b9984873ce1034c808507198
KyleDOwens <90162505+KyleDOwens@users.noreply.github.com>
2022-06-28 23:39:05 +0200

    Re-upload files

---
+++ CSCE_315_Project_3/customerScripts.js
@@ -0,0 +1,328 @@
+var isAccessible = false;
+
+
+function clearBoxes() {
+    document.getElementById("idSearch").value = "";
+    document.getElementById("quantitySearch").value = "";
+}
+
+
+function searchFunction(x) {
+    document.getElementById(x).focus();
+}
+
+
+function accessibleSwitch() {
+    if(isAccessible == false) {
+        document.getElementById("style").href = "customerStyleAccessible.css";
+        isAccessible = true;
+    }
+    else {
+        document.getElementById("style").href = "customerStyle.css";
+        isAccessible = false;
+    }
+}
+
+
+function deleteRow(btn) {
+    var row = btn.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+    updateOrderPrice();
+}
+
+
+function addOrderRow() {
+    var name;
+    var unitPrice;
+    var salePrice;
+    var id = document.getElementById('idSearch').value; 
+    var custQty = parseFloat(document.getElementById('quantitySearch').value);
+    var invQty;
+
+    // Do nto query the database if a field is empty
+    console.log("NAN CHECK:: ID = " + id + "\nCust Qty = " + custQty);
+    if (id == null || custQty == null || isNaN(id) || isNaN(custQty)) {
+        // TODO: Add feedback
+        console.log("A field is empty");
+        return;
+    }
+
+    // Use a promise to prevent the function from running before all data is recieved
+    var promise = new Promise( function(resolve, reject) {
+        // Query the database to get the food item info
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.responseType = "json";
+        xmlHttp.open("GET", "http://localhost:3000/foodItems");
+        xmlHttp.onload = function() {
+            // Status of the request is OK
+            if (xmlHttp.status == 200) {
+                resolve(xmlHttp.response);
+            }
+            // Request status not OK
+            else {
+                reject(Error(xmlHttp.statusText));
+            }
+
+            // Error with http request
+            xmlHttp.onerror = function() {
+                reject(Error("Data could not be retrieved"))
+            }
+        }
+        xmlHttp.send();
+    });
+
+    // Wait for the query to complete before working with the data
+    promise.then( 
+        function(data) {
+            name = data[id - 1].foodname;
+            invQty = data[id - 1].foodquantity;
+            unitPrice = data[id - 1].unitprice;
+
+            salePrice = custQty * unitPrice;
+            salePrice = salePrice.toFixed(2);
+            custQty = custQty.toFixed(2);
+
+            // Do not add a row if there is not enough quantity
+            if (custQty > invQty) {
+                // TODO: Add feedback
+                console.log("Not enough food in inventory");
+                return;
+            }
+
+            // Check to see if this food item is alread in the order
+            // If it is, update the old sale line instead of creating a new one
+            var table = document.getElementById("orders")
+            for (var i = 0; i < table.rows.length; i++) {
+                var cols = table.rows.item(i).cells;
+                saleFoodID = cols.item(1).innerHTML;
+
+                if (saleFoodID == id) {
+                    // Update the sale line values
+                    var newSalePrice = (parseFloat(cols.item(2).innerHTML) + parseFloat(salePrice)).toFixed(2);
+                    var newQty = (parseFloat(cols.item(3).innerHTML) + parseFloat(custQty)).toFixed(2);
+
+                    // Do not add more to the sale if there is not enough quantity
+                    if (newQty > invQty) {
+                        // TODO: Add feedback
+                        console.log("Not enough food in inventory");
+                        return;
+                    }
+                    
+                    cols.item(2).innerHTML = newSalePrice
+                    cols.item(3).innerHTML = newQty;
+
+                    //Clearing text boxes containing food ID and quantity
+                    clearBoxes();
+                    updateOrderPrice();
+                    return;
+                }
+            }
+
+            // If this is a new food item, create the new row in HTML
+            var tr = document.createElement('tr');
+
+            // Create all columns
+            rowText = ' \
+            <td>' + name + '</td> \
+            <td>' + id + '</td> \
+            <td>' + salePrice + '</td> \
+            <td>' + custQty + '</td> \
+            <td><button type="button" class="btn btn-outline-danger" onclick="deleteRow(this)">X</button></td> \
+            ';
+
+            tr.innerHTML = rowText;
+            table.appendChild(tr);
+
+            //Clearing text boxes containing food ID and quantity
+            clearBoxes();
+            updateOrderPrice();
+        },
+
+        function(error) {
+            console.log(error);
+        }
+    );
+}
+
+
+function completeCustomerOrder() {
+    var table = document.getElementById("orders");
+
+    // TODO: 
+    // Get the latest customerOrderID
+    // Get the latest saleLineID
+    //
+    // For each sale line in the table, 
+    //     Add the sale line to the CustomerSaleLine DB
+    //     Update the inventory in FoodItems DB
+    //     Sum up the total order cost
+    //
+    // Get the current date
+    // Get the payment info for the order
+    // Get the employeeID currently logged in
+    //
+    // Add the order to the CustomerOrder DB
+    //
+    // Update the cheatsheet
+
+    // Clear the HTML order table
+    var tableLen = table.rows.length;
+    for (var i = 1; i < tableLen; i++) {
+        table.deleteRow(1);
+    }
+    updateOrderPrice();
+}
+
+
+function cancelOrder() {
+    // Clear the HTML order table
+    // Do not need to interact with DB since changes are only made when an order is completed
+    var table = document.getElementById("orders");
+    var tableLen = table.rows.length;
+    for (var i = 1; i < tableLen; i++) {
+        table.deleteRow(1);
+    }
+    updateOrderPrice();
+}
+
+
+function updateInfoBar() {
+    var name;
+    var unitPrice;
+    var invQty;
+
+    // Get the current foodID
+    var id = document.getElementById('idSearch').value; 
+
+    // Query the foodItems database
+    // Use a promise to prevent the function from running before all data is recieved
+    var promise = new Promise( function(resolve, reject) {
+        // Query the database to get the food item info
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.responseType = "json";
+        xmlHttp.open("GET", "http://localhost:3000/foodItems");
+        xmlHttp.onload = function() {
+            // Status of the request is OK
+            if (xmlHttp.status == 200) {
+                resolve(xmlHttp.response);
+            }
+            // Request status not OK
+            else {
+                reject(Error(xmlHttp.statusText));
+            }
+
+            // Error with http request
+            xmlHttp.onerror = function() {
+                reject(Error("Data could not be retrieved"))
+            }
+        }
+        xmlHttp.send();
+    });
+
+    // Wait for the query to complete before working with the data
+    promise.then( 
+        function(data) {
+            // Do not attempt to read data if ID is invald
+            // TODO: Move this up depending on how database info is stored
+            if (id < 1 || id > data.length) {
+                console.log("Invalid ID input");
+                return;
+            }
+
+            // Get the food info
+            name = data[id - 1].foodname;
+            invQty = data[id - 1].foodquantity;
+            unitPrice = data[id - 1].unitprice;
+            
+            // Format food info into HTML
+            document.getElementById("itemInfoField").innerHTML = name + "<br>Unit Price = " + unitPrice + "&nbsp;&nbsp;&nbsp;&nbsp;Quantity = " + invQty;
+        },
+
+        function(error) {
+            console.log(error);
+        }
+    );
+}
+
+
+function createCheatSheet() {
+    // Query the entire foodItems database
+    // Use a promise to prevent the function from running before all data is recieved
+    var promise = new Promise( function(resolve, reject) {
+        // Query the database to get the food item info
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.responseType = "json";
+        xmlHttp.open("GET", "http://localhost:3000/foodItems");
+        xmlHttp.onload = function() {
+            // Status of the request is OK
+            if (xmlHttp.status == 200) {
+                resolve(xmlHttp.response);
+            }
+            // Request status not OK
+            else {
+                reject(Error(xmlHttp.statusText));
+            }
+
+            // Error with http request
+            xmlHttp.onerror = function() {
+                reject(Error("Data could not be retrieved"))
+            }
+        }
+        xmlHttp.send();
+    });
+
+    // Wait for the query to complete before working with the data
+    promise.then( 
+        function(data) {
+            var id;
+            var name;
+            var unitPrice;
+            var invQty;
+            var storage;
+
+            var table = document.getElementById("sheet");
+
+            // Add each food item to the cheat sheet
+            for (var i = 0; i < data.length; i++) {
+                console.log(data[i]);
+
+                var tr = document.createElement('tr');
+
+                id = data[i].foodid;
+                name = data[i].foodname;
+                unitPrice = data[i].unitprice.toFixed(2);
+                invQty = data[i].foodquantity.toFixed(2);
+                storage = data[i].storagetype;
+
+                // Create all columns
+                rowText = ' \
+                <td>' + id + '</td> \
+                <td>' + name + '</td> \
+                <td>€&nbsp;' + unitPrice + '</td> \
+                <td>' + invQty + '</td> \
+                <td>' + storage + '</td>';
+
+                tr.innerHTML = rowText;
+                table.appendChild(tr);
+            }
+        },
+
+        function(error) {
+            console.log(error);
+        }
+    );
+}
+
+
+function updateOrderPrice() {
+    var table = document.getElementById("orders");
+
+    var totalCost = "0.00";
+    for (var i = 1; i < table.rows.length; i++) {
+        var cols = table.rows.item(i).cells;
+        totalCost = parseFloat(totalCost) + parseFloat(cols.item(2).innerHTML);
+        totalCost = parseFloat(totalCost).toFixed(2);
+    }
+
+    document.getElementById("price").innerHTML = totalCost;
+}
---
+++ dbConnection.js
@@ -0,0 +1,151 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+var path = require('path');
+
+
+app.use(cors({origin: "*"}));
+app.use(express.static('CSCE_315_Project_3'));
+
+app.listen(port, () => {
+  console.log(`Point of Sales system listening on port ${port}`)
+})
+
+
+const {Client} = require('pg');
+const client = new Client({
+  host: "csce-315-db.engr.tamu.edu",
+  user: "csce315950_1user",
+  port: 5432,
+  password: "team1won",
+  database: "csce315950_1db"
+})
+
+
+var foodItems;
+client.connect();
+client.query('SELECT * FROM fooditems ORDER BY foodid ASC',(err, res)=>{
+  if (!err) {
+    foodItems = res.rows
+  } else {
+    console.log("\nERROR:");
+    console.log(err.message);
+  }
+  client.end();
+})
+
+/*var customerOrder;
+client.connect();
+client.query('SELECT * FROM customerorder ORDER BY customerorderid ASC',(err, res)=>{
+  if (!err) {
+    customerOrder = res.rows
+  } else {
+    console.log("\nERROR:");
+    console.log(err.message);
+  }
+  client.end();
+})
+
+var customerSaleLine;
+client.connect();
+client.query('SELECT * FROM customersaleline ORDER BY salelineid ASC',(err, res)=>{
+  if (!err) {
+    console.log(res.rows);
+    customerSaleLine = res.rows
+  } else {
+    console.log("\nERROR:");
+    console.log(err.message);
+  }
+  client.end();
+})
+
+var employeeList;
+client.connect();
+client.query('SELECT * FROM employeelist ORDER BY employeeid ASC',(err, res)=>{
+  if (!err) {
+    console.log(res.rows);
+    employeeList = res.rows
+  } else {
+    console.log("\nERROR:");
+    console.log(err.message);
+  }
+  client.end();
+})
+
+var vendorOrder;
+client.connect();
+client.query('SELECT * FROM vendororder ORDER BY vendororderid ASC',(err, res)=>{
+  if (!err) {
+    console.log(res.rows);
+    vendorOrder = res.rows
+  } else {
+    console.log("\nERROR:");
+    console.log(err.message);
+  }
+  client.end();
+})
+
+var vendorBuyLine;
+client.connect();
+client.query('SELECT * FROM vendorbuyline ORDER BY vendorlineid ASC',(err, res)=>{
+  if (!err) {
+    console.log(res.rows);
+    vendorBuyLine = res.rows
+  } else {
+    console.log("\nERROR:");
+    console.log(err.message);
+  }
+  client.end();
+})*/
+
+
+app.get('/foodItems', function(req, res){
+  res.send(foodItems);
+});
+
+/*app.get('/customerOrder', function(req, res){
+  res.send(customerOrder);
+});
+
+app.get('/customerSaleLine', function(req, res){
+  res.send(customerSaleLine);
+});
+
+app.get('/employeeList', function(req, res){
+  res.send(employeeList);
+});
+
+app.get('/vendorOrder', function(req, res){
+  res.send(vendorOrder);
+});
+
+app.get('/vendorBuyLine', function(req, res){
+  res.send(vendorBuyLine);
+});
+
+app.post('/addCustomerOrder', function(req, res) {
+  orderID = req.body.customerorderid;
+  orderDate = req.body.customerorderdate;
+  orderTotal = req.body.customerordertotal;
+  payment = req.body.paymentmethod;
+  employeeID = req.body.employeeid;
+  
+  var vendorBuyLine;
+  client.connect();
+  var command = 'INSERT INTO customerorder VALUES(' + orderID +', ' + orderDate +', ' + orderTotal +', ' + payment +', ' + employeeID + ')';
+  client.query(command, (err, res)=>{
+    if (!err) {
+      console.log("COMPLETE\n\n");
+    } else {
+      console.log("\nERROR:");
+      console.log(err.message);
+    }
+    client.end();
+  })
+});
+
+// Post = new
+// Put = edit
+// Delete
+*/
