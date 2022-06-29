f039dc39e6ab54eb6db50ae855e3c8171f97e932
KyleDOwens <90162505+KyleDOwens@users.noreply.github.com>
2022-06-30 00:16:35 +0200

    Updated reportScripts.js

--- projectFiles/reportScripts.js
+++ projectFiles/reportScripts.js
@@ -17,6 +17,7 @@ async function salesReport() {
         body: JSON.stringify(dateInfo)
     });
     const saleLineDB = await saleResponse.json();
+    console.log(saleLineDB);
 
     if (saleLineDB == "-1") {
         console.log("Invalid dates");
@@ -89,4 +90,88 @@ async function salesReport() {
 
     // Sum up the total revenue
     document.getElementById("revenue").innerHTML = totalRevenue;
+}
+
+
+async function excessReport() {
+    console.log("\nRUNNING EXCESS REPORT");
+
+    const dateInfo = {
+        startdate: document.getElementById("startdate").value,
+        enddate: document.getElementById("enddate").value
+    };
+    console.log(dateInfo);
+
+    // Query to get the customerSaleLine database
+    const saleResponse = await fetch('/salesReport', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dateInfo)
+    });
+    const saleLineDB = await saleResponse.json();
+    console.log("SaleLineDB:");
+    console.log(saleLineDB);
+
+    if (saleLineDB == "-1") {
+        console.log("Invalid dates");
+        // TODO: Add feedback
+        return;
+    }
+
+    
+    // Query to get the foodItems database
+    const foodResponse = await fetch('/foodItems', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    });
+    const foodItemsDB = await foodResponse.json();
+
+    // Create structues to hold the data
+    var foodQuantities;
+    (foodQuantities = []).length = foodItemsDB.length; 
+    foodQuantities.fill(0.00);
+
+    
+    // For each sale line, sum up the quantity sold
+    for (var i = 0; i < saleLineDB.length; i++) {
+        var saleFoodID = saleLineDB[i].foodid;
+        var saleQuantity = saleLineDB[i].salelinequantity;
+
+        foodQuantities[saleFoodID - 1] += parseFloat(saleQuantity);
+
+        console.log("quantity sold: ");
+        console.log(saleQuantity);
+    }
+    console.log(foodQuantities);
+
+    
+    // Clear the report results table
+    var table = document.getElementById("report");
+    var tableLen = table.rows.length;
+    for (var i = 1; i < tableLen; i++) {
+        table.deleteRow(1);
+    }
+
+    // Fill the table with the report results
+    var table = document.getElementById("report");
+    for (var i = 0; i < foodItemsDB.length; i++) {
+        // Create the new row
+        var tr = document.createElement('tr');
+
+        // Create all columns and fill them with data
+        rowText = ' \
+        <td>' + (i + 1) + '</td> \
+        <td>' + foodItemsDB[i].foodname + '</td> \
+        <td>' + foodQuantities[i].toFixed(2) + '</td> \
+        <td>' + foodItemsDB[i].foodquantity + '</td>';
+
+        tr.innerHTML = rowText;
+        table.appendChild(tr);
+    }
 }
