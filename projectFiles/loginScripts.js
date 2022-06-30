6189de906dda55fcd8124ed5ef77c79a3ad115dd
Harrison Kuhn <90007832+The10thDoc@users.noreply.github.com>
2022-06-30 11:12:50 +0200

    Moved login functions into separate file
    Separate file now contains login functions, not relying on customerScripts anymore

---
+++ projectFiles/loginScripts.js
@@ -0,0 +1,33 @@
+async function checkPassword() {
+
+    // Query the database to get the employee table
+    var employeeid = document.getElementById("userNameInput").value;
+    var enteredPassword = document.getElementById("passwordInput").value;
+
+    const employeeInformation = {
+        employeeID: parseInt(employeeid)
+    }
+    
+    // Query the employeeList for a specific employeeid
+    const response = await fetch('/employeeList', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(employeeInformation)
+    });
+    const data = await response.json();
+
+    var storedPassword = data.password;
+    
+    if(enteredPassword === storedPassword) {
+        document.getElementById("loginButton").href = "customer.html";
+        document.getElementById("loginCheck").innerHTML = "";
+    }
+    else {
+        clearLoginBoxes();
+        document.getElementById("loginCheck").innerHTML = "Incorrect Username/Password.";
+
+    }
+}
